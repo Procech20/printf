bef7ab6acffd9af0779f68d8fe44e1187ca71cb7
C-distin <paannang@st.edu.gh>
2021-10-05 17:51:14 +0000

    Main Changes

--- main.h
+++ main.h
@@ -1,32 +1,47 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-#include <stdarg.h>
-/**
- * struct print - structure for printing various types
- * @t: type to print
- * @f: function to print
- */
-typedef struct print
-{
-	char *t;
-	int (*f)(va_list);
-} print_t;
+/* import some libraries */
 
-int _putchar(char c);
+#include <stdarg.h>
+#include <stdio.h>
+#include <unistd.h>
+#include <limits.h>
+#include <stdlib.h>
+
+#define BUFFSIZE 1024
+
+/**
+ * struct convert - defines a struct for symbols and functions
+ *
+ * @sym: The operator
+ * @f: the function associated to symbol
+ */
+
+typedef struct convert
+{
+	char *sym;
+	int (*f)(va_list);
+} convert_t;
+
+/*Main functions*/
+int parser(const char *format, convert_t f_list[], va_list arg_list);
 int _printf(const char *format, ...);
-int print_c(va_list c);
-int print_s(va_list s);
-int print_i(va_list i);
-int print_d(va_list d);
-int print_u(va_list u);
-int print_b(va_list b);
-int print_o(va_list o);
-int print_x(va_list x);
-int print_X(va_list X);
-int print_p(va_list p);
-int print_S(va_list S);
-int print_r(va_list r);
-int print_R(va_list R);
+int _putchar(char c);
+int print_char(va_list);
+int print_string(va_list);
+int print_percent(__attribute__((unused))va_list);
+int print_integer(va_list list);
+int print_decimal(va_list list);
+int rot_13(va_list list);
+int print_octal(va_list list);
+int print_unint(va_list list);
+int print_binary(va_list list);
+int binary_recursive(unsigned int num, int len);
+int _hex_str(unsigned int n, unsigned int hex, char alpha);
+int _hex_l(va_list list);
+int _hex_u(va_list list);
+int str(va_list list);
+int rev_string(__attribute__((unused))va_list list, char *s, ...);
 
 #endif /* MAIN_H */
